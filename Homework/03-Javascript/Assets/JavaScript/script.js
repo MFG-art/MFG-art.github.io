6e28b67dd2c2f5aad1135f40123f46a08bee24a0
Mauricio Fuentes Gomez <fuentesmauricio75@gmail.com>
2019-09-30 18:35:33 -0500

    Edited .js file (while and for loop)

--- Homework/03-Javascript/Assets/JavaScript/script.js
+++ Homework/03-Javascript/Assets/JavaScript/script.js
@@ -2,8 +2,28 @@
 //  64 - 90 are uppercase letters A - Z
 // 97 - 122 are lowercase letters a - z
 var passwordLength;
+passwordLength = prompt("Enter your password length (as a number)");
+passwordLength = parseInt(passwordLength);
+
+while (passwordLength < 8 || passwordLength > 128) {
+    if (passwordLength <8) {
+        alert("Password must be at least 8 characters long!");
+        passwordLength = prompt("Enter a new password length");
+        passwordLength = parseInt(passwordLength);
+    }
+    else if (passwordLength > 128) {
+        alert("Password cannot be longer than 128 characters!");
+        passwordLength = prompt("Enter a new password length");
+        passwordLength = parseInt(passwordLength);
+    }
+}
+
+for (var i = 0; i < passwordLength; i++){
+    var randCharType = Math.floor(Math.random()*3)
+}
+
 // generates random lowercase letter as ASCII value
-Math.floor(Math.random()*26)+97; 
+var newChar = Math.floor(Math.random()*26)+97; 
 
 //generates random Uppercase letter as ASCII value
 Math.floor(Math.random()*26)+64; 
