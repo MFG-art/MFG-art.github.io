147ed8f3911eb7288a3ae0fdeddea3fb390ebc54
Mauricio Fuentes Gomez <fuentesmauricio75@gmail.com>
2019-09-30 18:20:28 -0500

    created script.js file

---
+++ Homework/03-Javascript/Assets/JavaScript/script.js
@@ -0,0 +1,12 @@
+// 48 - 57 are numbers 0 - 9
+//  64 - 90 are uppercase letters A - Z
+// 97 - 122 are lowercase letters a - z
+var passwordLength;
+// generates random lowercase letter as ASCII value
+Math.floor(Math.random()*26)+97; 
+
+//generates random Uppercase letter as ASCII value
+Math.floor(Math.random()*26)+64; 
+
+// generates random number (0-9) as ASCII value
+Math.floor(Math.random*10)+48;
