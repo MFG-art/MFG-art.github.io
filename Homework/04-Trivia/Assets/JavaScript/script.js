7e04d74fe2933dfbcfef17152df2cb79164e3e98
Mauricio Fuentes Gomez <fuentesmauricio75@gmail.com>
2019-10-12 14:46:51 -0500

    Worked on js functions

--- Homework/04-Trivia/Assets/JavaScript/script.js
+++ Homework/04-Trivia/Assets/JavaScript/script.js
@@ -1,5 +1,10 @@
 // THIS FUNCTION GENERATES THE HEADER, WITH TIME = X
-function generateHeader(x,timeCount){
+var time = 0;
+var playerPoints = 0;
+var timerSpan = $("<span>");
+var timeCount = false;
+
+function generateHeader(timeCount){
     // actual link to high scores
     var highScoreLink = $("<a>");
     highScoreLink.text("view high scores");
@@ -12,8 +17,6 @@ function generateHeader(x,timeCount){
     highScoreDiv.append(highScoreLink);
 
     // timer display
-    time = x;
-    var timerSpan = $("<span>");
     timerSpan.attr("style","line-height:200%;margin:10px;");
     timerSpan.html("Time left: " + time);
 
@@ -32,14 +35,19 @@ function generateHeader(x,timeCount){
 
     if (timeCount === true){
         var timeCountdown = setInterval(function(){
-            time--;
+            
             timerSpan.html("Time left: " + time);
+
+            if (time < 0) {
+                timeCount = false;
+            }
 
     
             if (time === 0){
                 clearInterval(timeCountdown);
                 alert("Time's Up!");
             }
+            time--;
         }, 1000);
     }
 }
@@ -47,7 +55,8 @@ function generateHeader(x,timeCount){
 // THIS FUNCTION GENERATES THE START SCREEN (TIME = 0)
 function startScreen(){
 
-    generateHeader(0,false);
+
+    generateHeader(false);
 
     //creates the main text for the intro screen
     var mainTxt = $("<h3>");
@@ -93,12 +102,22 @@ var answers2 = ["Square","Low Pass Filter","Master Saw"];
 var answers3 = ["Circle","Band Pass Filter","Epic Saw"];
 var answers4 = ["Sawtooth","Notch Filter","Supersaw"];
 
+questionNum = 0;
+var button1points = [0,0,0];
+var button2points = [0,1,0];
+var button3points = [1,0,0];
+var button4points = [0,0,1];
+questionNum = 0;
+
 function questions(){
+    
+    if (questionNum === 0) {
+        time = 60;
+    }
 
-        
-    generateHeader(60,true);
+    generateHeader(true);
 
-    questionNum = 0;
+    
 
     currentQuestion = $("<h1>");
     $(document.body).append(currentQuestion);
@@ -114,21 +133,45 @@ function questions(){
     nextQuestion();
 
     button1.on("click",function(){
+        playerPoints += button1points[questionNum];
+        if (button1points[questionNum] === 0) {
+            time = time - 10;
+            console.log(time);
+            timerSpan.html("Time left: " + time);
+        }
         questionNum++;
         nextQuestion();
     });
 
     button2.on("click",function(){
+        playerPoints += button2points[questionNum];
+        if (button2points[questionNum] === 0) {
+           time = time - 10;
+           console.log(time);
+           timerSpan.html("Time left: " + time);
+        }
         questionNum++;
         nextQuestion();
     });
 
     button3.on("click",function(){
+        playerPoints += button3points[questionNum];
+        if (button3points[questionNum] === 0) {
+            time = time - 10;
+            console.log(time);
+            timerSpan.html("Time left: " + time);
+        }
         questionNum++;
         nextQuestion();
     });
 
     button4.on("click",function(){
+        playerPoints += button4points[questionNum];
+        if (button4points[questionNum] === 0) {
+            time = time - 10;
+            console.log(time);
+            timerSpan.html("Time left: " + time);
+        }
         questionNum++;
         nextQuestion();
     });
@@ -142,6 +185,36 @@ function nextQuestion(){
         button2.text(answers2[questionNum]);
         button3.text(answers3[questionNum]);
         button4.text(answers4[questionNum]);
+        
+        if (questionNum >= 3) {
+            time = 0;
+            $("button").remove();
+            $("h1").remove();
+            $("#header-div").remove();
+            $("#main-text").remove();
+            $("#start-button").remove();
+           
+            
+        }
 }
 
 startScreen();
+
+function enterUsername () {
+    alert(playerPoints);
+    var username = prompt("Enter username: ");
+    var scoreboard = {
+        "name":username,
+        "score":playerPoints,
+    }
+    localStorage.setItem(playerPoints,JSON.stringify(scoreboard));
+    time = 0;
+    questionNum = 0;
+    playerPoints = 0;
+    highScoreScreen();
+}
+
+function highScoreScreen(){
+    alert("Welcome to the high score screen!");
+    startScreen();
+}
