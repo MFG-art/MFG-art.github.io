a250835b378810555e263df281a78a26df7f8f17
Mauricio Fuentes Gomez <fuentesmauricio75@gmail.com>
2019-10-21 12:45:09 -0500

    Distinguishing ajax calls now works. uses success: and error: in call.

--- Homework/06-Weather/Assets/script.js
+++ Homework/06-Weather/Assets/script.js
@@ -9,23 +9,25 @@ var city, date, iconImage, temp, humidity, windSpeed, uvIndex;
 
 $("#submitBtn").on("click",function(event){
     event.preventDefault();
+    
     userInput = $("#search").val()
+    // Trims user input and replaces spaces with +.
     userInput = userInput.trim();
-
     while (userInput.includes(" ")){
         userInput = userInput.replace(" ","+");
-        console.log(userInput);
     }
 
     var APIKey = "1b1afd411484586400e59a0b2f0bbe81";
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ userInput + "&appid=" + APIKey;
 
-
+    var APIKey = "1b1afd411484586400e59a0b2f0bbe81";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ userInput + "&appid=" + APIKey;
     var ajaxCall;
 
     $.ajax({
         url: queryURL,
         method: "GET",
+        // if ajax call was 
         success: function(data){
             ajaxCall = data;
             console.log(ajaxCall);
@@ -38,17 +40,14 @@ $("#submitBtn").on("click",function(event){
 
   
     .then(function(response){
-        console.log(response);
         city = response.name;
         date = response.dt;
         temp = response.main.temp;
-        console.log(temp);
         // converting temp to Fahrenheit
         temp = (((temp-273.15)*(9/5))+32);
         temp = temp.toFixed(0);
-        console.log(temp);
         humidity = response.main.humidity;
     });
 
-
+    
 });
