a1fdf553419f5a2663d28a3378b5fbbd3ba2597f
Mauricio Fuentes Gomez <fuentesmauricio75@gmail.com>
2019-10-21 12:18:13 -0500

    Code now distinguishes valid and invalid AJAX calls.

--- Homework/06-Weather/Assets/script.js
+++ Homework/06-Weather/Assets/script.js
@@ -1,7 +1,54 @@
-var APIKey = "1b1afd411484586400e59a0b2f0bbe81";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=South+Saint+Paul,US&appid=" + APIKey;
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {});
+var userInput;
+
+
+
+
+var city, date, iconImage, temp, humidity, windSpeed, uvIndex;
+
+
+$("#submitBtn").on("click",function(event){
+    event.preventDefault();
+    userInput = $("#search").val()
+    userInput = userInput.trim();
+
+    while (userInput.includes(" ")){
+        userInput = userInput.replace(" ","+");
+        console.log(userInput);
+    }
+
+    var APIKey = "1b1afd411484586400e59a0b2f0bbe81";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ userInput + "&appid=" + APIKey;
+
+
+    var ajaxCall;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        success: function(data){
+            ajaxCall = data;
+            console.log(ajaxCall);
+        },
+        error: function(data){
+            console.log("Error!");
+            alert("Error! Please enter a valid city name.");
+        },
+    })
+
+  
+    .then(function(response){
+        console.log(response);
+        city = response.name;
+        date = response.dt;
+        temp = response.main.temp;
+        console.log(temp);
+        // converting temp to Fahrenheit
+        temp = (((temp-273.15)*(9/5))+32);
+        temp = temp.toFixed(0);
+        console.log(temp);
+        humidity = response.main.humidity;
+    });
+
+
+});
