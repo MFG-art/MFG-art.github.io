bbb72dd8544b1bcb441db0eab05ec0cad669e37a
Mauricio Fuentes Gomez <fuentesmauricio75@gmail.com>
2019-10-28 09:17:43 -0500

    Added ajax call for uv index. Weather results now display index value.

--- Homework/06-Weather/Assets/script.js
+++ Homework/06-Weather/Assets/script.js
@@ -1,16 +1,14 @@
 //Variable used to store user input
-var userInput;
 var pastSearches = [];
 var cityNames = [];
 var pushToArray = false;
 var pastSearchesObject = {};
-console.log(pastSearches.length);
-
-updateButtons();
 
 //Variables used for display purposes
 var city, date, iconImage, temp, humidity, windSpeed, uvIndex;
 var responseTrue;
+
+updateButtons();
 
 function updateButtons() {
   $(".grayColor").empty();
@@ -18,8 +16,9 @@ function updateButtons() {
     pastSearchesObject = localStorage.getItem("pastSearchesJSON");
     pastSearchesObject = JSON.parse(pastSearchesObject);
     pastSearches = pastSearchesObject.pastSearches;
-    console.log(pastSearches);
     cityNames = pastSearchesObject.cityNames;
+    console.log(pastSearches);
+    console.log(cityNames);
 
     for (var i = 0; i < pastSearches.length; i++) {
       var newBtn = $("<button>");
@@ -27,27 +26,19 @@ function updateButtons() {
       newBtn.attr("class", "newButton");
       $(".grayColor").append(newBtn);
     }
+  } else {
+    return;
   }
 }
 
-$("#submitBtn").on("click", function(event) {
-  event.preventDefault();
-
-  userInput = $("#search").val();
-  // Trims user input and replaces spaces with +.
-  userInput = userInput.trim();
-  userInput = userInput.toLowerCase();
-  while (userInput.includes(" ")) {
-    userInput = userInput.replace(" ", "+");
-  }
-
-  //API key and URL needed for first AJAX call
+function getWeather(userInput) {
   var APIKey = "1b1afd411484586400e59a0b2f0bbe81";
   var queryURL =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     userInput +
     "&appid=" +
     APIKey;
+  let lat, long;
 
   $.ajax({
     url: queryURL,
@@ -67,6 +58,8 @@ $("#submitBtn").on("click", function(event) {
   }).then(function(response) {
     city = response.name;
     date = response.dt;
+    lat = response.coord.lat;
+    long = response.coord.lon;
     iconImage = response.weather[0].icon;
 
     temp = response.main.temp;
@@ -78,10 +71,36 @@ $("#submitBtn").on("click", function(event) {
     windSpeed = response.wind.speed;
   });
 
+  setTimeout(function() {
+    queryURL =
+      "http://api.openweathermap.org/data/2.5/uvi?appid=" +
+      APIKey +
+      "&lat=" +
+      lat +
+      "&lon=" +
+      long;
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+      // if ajax call was successful
+      success: function(data) {
+        responseTrue = true;
+        uvIndex = data.value;
+        ajaxCall = data;
+        console.log(ajaxCall);
+      },
+      error: function(data) {
+        responseTrue = false;
+        console.log("Error!");
+        alert("Error! Please enter a valid city name.");
+      }
+    });
+  }, 300);
+
   //Waits for AJAX call to end.
   setTimeout(function() {
     if (responseTrue) {
-      //
+      //Checks if the array is not empty and compares each item in the array with the current user input
       if (pastSearches.length > 0) {
         for (var i = 0; i < pastSearches.length; i++) {
           if (pastSearches[i] !== userInput) {
@@ -91,6 +110,7 @@ $("#submitBtn").on("click", function(event) {
             break;
           }
         }
+        //If the array is empty, push the new user input
       } else {
         pushToArray = true;
       }
@@ -103,6 +123,7 @@ $("#submitBtn").on("click", function(event) {
           pastSearches: pastSearches,
           cityNames: cityNames
         };
+        console.log(pastSearchesObject);
         let pastSearchesJSON = JSON.stringify(pastSearchesObject);
         localStorage.setItem("pastSearchesJSON", pastSearchesJSON);
         console.log(pastSearches);
@@ -136,6 +157,8 @@ $("#submitBtn").on("click", function(event) {
       humiditySpan.attr("class", "humiditySpan");
       windSpeedSpan.text("Wind speed: " + windSpeed);
       windSpeedSpan.attr("class", "windSpeedSpan");
+      UVIndexSpan.text("UV Index: " + uvIndex);
+      UVIndexSpan.attr("class", "UVIndexSpan");
 
       weatherDiv.append(citySpan);
       weatherDiv.append(dateSpan);
@@ -143,120 +166,35 @@ $("#submitBtn").on("click", function(event) {
       weatherDiv.append(tempSpan);
       weatherDiv.append(humiditySpan);
       weatherDiv.append(windSpeedSpan);
+      weatherDiv.append(UVIndexSpan);
       $(".lightBlueColor").append(weatherDiv);
     }
-  }, 300);
-});
+  }, 700);
+}
 
-$(".newButton").on("click", function(event) {
+$("#submitBtn").on("click", function(event) {
   event.preventDefault();
 
-  userInput = $(this).text();
+  let userInput = $("#search").val();
+  // Trims user input and replaces spaces with +.
+  userInput = userInput.trim();
+  userInput = userInput.toLowerCase();
   while (userInput.includes(" ")) {
     userInput = userInput.replace(" ", "+");
   }
+  getWeather(userInput);
+});
 
-  //API key and URL needed for first AJAX call
-  var APIKey = "1b1afd411484586400e59a0b2f0bbe81";
-  var queryURL =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    userInput +
-    "&appid=" +
-    APIKey;
-
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-    // if ajax call was successful
-    success: function(data) {
-      responseTrue = true;
-      ajaxCall = data;
-      console.log(ajaxCall);
-    },
-    error: function(data) {
-      responseTrue = false;
-      console.log("Error!");
-
-      alert("Error! Please enter a valid city name.");
-    }
-  }).then(function(response) {
-    city = response.name;
-    date = response.dt;
-    iconImage = response.weather[0].icon;
-
-    temp = response.main.temp;
-    // converting temp to Fahrenheit
-    temp = (temp - 273.15) * (9 / 5) + 32;
-    temp = temp.toFixed(0);
-
-    humidity = response.main.humidity;
-    windSpeed = response.wind.speed;
-  });
-
-  //Waits for AJAX call to end.
-  setTimeout(function() {
-    if (responseTrue) {
-      //
-      if (pastSearches.length > 0) {
-        for (var i = 0; i < pastSearches.length; i++) {
-          if (pastSearches[i] !== userInput) {
-            pushToArray = true;
-          } else {
-            pushToArray = false;
-            break;
-          }
-        }
-      } else {
-        pushToArray = true;
-      }
-
-      if (pushToArray) {
-        pastSearches.push(userInput);
-        console.log(pastSearches);
-        pastSearchesObject = { pastSearches: pastSearches };
-        let pastSearchesJSON = JSON.stringify(pastSearchesObject);
-        localStorage.setItem("pastSearchesJSON", pastSearchesJSON);
-        console.log(pastSearches);
-        updateButtons();
-      }
-
-      var weatherDiv = $("<div>");
-      var citySpan = $("<span>");
-      var dateSpan = $("<span>");
-      var iconImgEL = $("<img>");
-      var tempSpan = $("<span>");
-      var humiditySpan = $("<span>");
-      var windSpeedSpan = $("<span>");
-      var UVIndexSpan = $("<span>");
-
-      citySpan.text(city);
-      citySpan.attr("class", "citySpan");
-      var dateMoment = moment(date, "X");
-      date = dateMoment.format("LL");
-      dateSpan.text(date);
-      dateSpan.attr("class", "dateSpan");
-      iconImgEL.attr("alt", iconImage);
-      iconImgEL.attr(
-        "src",
-        "http://openweathermap.org/img/wn/" + iconImage + "@2x.png"
-      );
-
-      tempSpan.text(temp + "°F");
-      tempSpan.attr("class", "tempSpan");
-      humiditySpan.text("Humidity: " + humidity + " %");
-      humiditySpan.attr("class", "humiditySpan");
-      windSpeedSpan.text("Wind speed: " + windSpeed);
-      windSpeedSpan.attr("class", "windSpeedSpan");
-
-      weatherDiv.append(citySpan);
-      weatherDiv.append(dateSpan);
-      weatherDiv.append(iconImgEL);
-      weatherDiv.append(tempSpan);
-      weatherDiv.append(humiditySpan);
-      weatherDiv.append(windSpeedSpan);
-      $(".lightBlueColor").append(weatherDiv);
-    }
-  }, 300);
+$(document).on("click", ".newButton", function(event) {
+  event.preventDefault();
+  let userInput = $(this).text();
+  userInput = userInput.trim();
+  userInput = userInput.toLowerCase();
+  while (userInput.includes(" ")) {
+    userInput = userInput.replace(" ", "+");
+  }
+  console.log(userInput);
+  getWeather(userInput);
 });
 
 updateButtons();
