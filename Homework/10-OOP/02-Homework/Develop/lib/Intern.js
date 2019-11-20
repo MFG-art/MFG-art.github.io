4d9fbfb0d6da55a52dc01b122d42c92d7df4faca
Mauricio Fuentes Gomez <fuentesmauricio75@gmail.com>
2019-11-20 16:14:06 -0600

    Finished coding Intern class; it now passes it's test suite

---
+++ Homework/10-OOP/02-Homework/Develop/lib/Intern.js
@@ -0,0 +1,19 @@
+const Employee = require("./Employee");
+
+class Intern extends Employee {
+  constructor(name, id, email, school) {
+    super(name, id, email);
+    this.school = school;
+    this.role = "Intern";
+  }
+
+  getRole() {
+    return this.role;
+  }
+
+  getSchool() {
+    return this.school;
+  }
+}
+
+module.exports = Intern;
