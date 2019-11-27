b1c07830e61b4a92fe7d6f1d7b9d4751f6b3fb6d
Mauricio Fuentes Gomez <fuentesmauricio75@gmail.com>
2019-11-27 12:01:49 -0600

    implemented HTML file generation

--- Homework/10-OOP/02-Homework/Develop/app.js
+++ Homework/10-OOP/02-Homework/Develop/app.js
@@ -102,11 +102,106 @@ function createObjects() {
         break;
     }
   });
-  console.log(teamMembers);
+  try {
+    console.log(teamMembers);
+    createHTML();
+  } catch {
+    createHTML();
+  }
 }
 
-let divTemplate = `
-<div>
+function createHTML() {
+  const htmlHeader = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Team Member Chart</title>
+    <style>
+      body {
+        background-color: tan;
+      }
+      h1 {
+        margin: 10px 100px 0px 100px;
+        text-align: center;
+        font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+        background-color: whitesmoke;
+        border: 1 px darkgrey;
+        border-radius: 5px;
+        padding: 10px;
+      }
+      .main {
+        margin: 50px 100px;
+        background-color: gray;
+        border: 1 px darkgrey;
+        border-radius: 5px;
+        padding: 5px;
+        overflow: auto;
+      }
+      .new-member {
+        font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+        float: left;
+        margin: 20px;
+        background-color: honeydew;
+        border: 1 px darkgrey;
+        border-radius: 5px;
+        padding: 5px;
+        width: 25%;
+        text-align: center;
+      }
+    </style>
+  </head>
+  <body>
+  <h1>Team Members:</h1>
+  <div class="main">
+    `;
 
+  const htmlFooter = `
+  </div>
+</div>
+</body>
+</html>`;
 
-`;
+  fs.writeFileSync("./team-members.html", htmlHeader, "utf8");
+
+  promptData.forEach(member => {
+    switch (member.role) {
+      case "Manager":
+        var memberDiv = `
+        <div class="new-member">
+          <h2>${member.name} - ${member.role}</h2>
+          <p>ID: ${member.id}</p>
+          <p>email: ${member.email}</p>
+          <p>Office number: ${member.other}</p>
+        </div>
+        `;
+        break;
+      case "Engineer":
+        var memberDiv = `
+        <div class="new-member">
+          <h2>${member.name} - ${member.role}</h2>
+          <p>ID: ${member.id}</p>
+          <p>email: ${member.email}</p>
+          <p>Github account: ${member.other}</p>
+        </div>
+        `;
+        break;
+      case "Intern":
+        var memberDiv = `
+        <div class="new-member">
+          <h2>${member.name} - ${member.role}</h2>
+          <p>ID: ${member.id}</p>
+          <p>email: ${member.email}</p>
+          <p>School: ${member.other}</p>
+        </div>
+        `;
+        break;
+      default:
+        break;
+    }
+    fs.appendFileSync("./team-members.html", memberDiv, "utf8");
+  });
+  fs.appendFileSync("./team-members.html", htmlFooter, "utf8");
+}
