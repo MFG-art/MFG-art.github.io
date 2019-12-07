628ed5b02ad1357f8cf81d1d93d65c47846af4f0
Mauricio Fuentes Gomez <fuentesmauricio75@gmail.com>
2019-12-07 15:41:30 -0600

    Created schema sql file

---
+++ Homework/12-Mysql/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS cms_db;
+CREATE DATABASE cms_db;
+USE cms_db;
+
+CREATE TABLE employees (
+id INT AUTO_INCREMENT NOT NULL,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL,
+manager_id INT NULL,
+PRIMARY KEY (`id`)
+); 
+
+CREATE TABLE roles (
+id INT AUTO_INCREMENT NOT NULL,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL(10,2) NOT NULL,
+department_id INT NOT NULL,
+PRIMARY KEY (`id`)
+)
+
+CREATE TABLE departments (
+id INT AUTO_INCREMENT NOT NULL,
+name VARCHAR(30),
+PRIMARY KEY (`id`)
+)
+
