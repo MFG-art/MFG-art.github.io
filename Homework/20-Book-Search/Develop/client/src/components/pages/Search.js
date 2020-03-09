be92463b217dde2f565b59b9911237b9b59811f4
Mauricio Fuentes Gomez <fuentesmauricio75@gmail.com>
2020-03-09 18:19:54 -0500

    Created Search and Saved class components

---
+++ Homework/20-Book-Search/Develop/client/src/components/pages/Search.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Search;
