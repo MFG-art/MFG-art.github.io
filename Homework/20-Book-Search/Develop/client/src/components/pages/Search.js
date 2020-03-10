6ffa855aa36ae0b2bc962c1c2b9d1e1015491d8a
Mauricio Fuentes Gomez <fuentesmauricio75@gmail.com>
2020-03-10 17:29:39 -0500

    Added bootstrap components to Search and Saved react components

--- Homework/20-Book-Search/Develop/client/src/components/pages/Search.js
+++ Homework/20-Book-Search/Develop/client/src/components/pages/Search.js
@@ -9,7 +9,78 @@ class Search extends React.Component {
   }
 
   render() {
-    return <div></div>;
+    return (
+      <div>
+        {/* NAVBAR CODE */}
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">
+            Google Books
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="/">
+                  Search <span className="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/saved">
+                  Saved
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        {/* JUMBOTRON 1 / HEADER */}
+        <div className="jumbotron m-5">
+          <h1 className="display-4">(REACT) GOOGLE BOOKS SEARCH</h1>
+          <p className="lead">Search for and save books that interest you!</p>
+        </div>
+
+        {/* JUMBOTRON 2 */}
+        <div className="jumbotron m-5">
+          <h1 className="display-4">Book Search</h1>
+          <hr className="my-4" />
+          <p className="lead">Book: </p>
+          <form>
+            <div class="form-group">
+              <label for="bookSearch">Search for book:</label>
+              <input
+                type="text"
+                class="form-control"
+                id="bookSearch"
+                aria-describedby="emailHelp"
+              />
+              <small id="emailHelp" class="form-text text-muted">
+                Click the submit button to receive information about your book.
+              </small>
+            </div>
+            <button type="submit" class="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+
+        {/* RESULTS JUMBOTRON */}
+        <div className="jumbotron m-5">
+          <h1 className="display-4">Results</h1>
+          <p className="lead">Here are the results from Google Books:</p>
+        </div>
+      </div>
+    );
   }
 }
 
